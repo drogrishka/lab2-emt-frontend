bfa83b5cec26f13c0ef7b08957ed44e22d885892
drogrishka <ana.drogriska@hotmail.com>
2022-04-25 22:51:28 +0200

    Initial commit

---
+++ src/components/Header/header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const header = (props) => {
+    return (
+        <header>
+            <nav className="navbar navbar-dark bg-dark">
+                <div className="container">
+                    <a className="navbar-brand" href="/books">EMT Lab 2 - 191024</a>
+                    <div className="d-flex ">
+                        <Link className="nav-link text-white" to={"/books"}>Books</Link>
+                        <Link className="nav-link text-white" to={"/authors"}>Authors</Link>
+                        <Link className="nav-link text-white" to={"/categories"}>Categories</Link>
+                        <Link className={"nav-link text-white"} to={"/countries"}>Countries</Link>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+
+}
+export default header;
